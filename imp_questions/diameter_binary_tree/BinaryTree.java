d22d53e266cd7259b291a9e890f208ffed244098
KrishBawangade <krishbawangade08@gmail.com>
2026-05-25 08:12:23 +0530

    Diameter of a Binary Tree

---
+++ imp_questions/diameter_binary_tree/BinaryTree.java
@@ -0,0 +1,99 @@
+package imp_questions.diameter_binary_tree;
+
+import java.util.*;
+
+public class BinaryTree {
+    TreeNode root;
+    int diameter = 0;
+
+    public BinaryTree(){
+
+    }
+
+    public void populate(Scanner sc){
+        System.out.print("Enter the root node: ");
+        int value = sc.nextInt();
+        root = new TreeNode(value);
+
+        populate(sc, root);
+    }
+
+    private void populate(Scanner sc, TreeNode node){
+        System.out.printf("Do you want to enter left of %d: ", node.val);
+        boolean isLeft = sc.nextBoolean();
+
+        if(isLeft){
+            System.out.print("Enter the value of the left node: ");
+            int value = sc.nextInt();
+            TreeNode left = new TreeNode(value);
+            node.left = left;
+            populate(sc, left);
+        }
+
+        System.out.printf("Do you want to enter right of %d: ", node.val);
+        boolean isRight = sc.nextBoolean();
+
+        if(isRight){
+            System.out.print("Enter the value of the right node: ");
+            int value = sc.nextInt();
+            TreeNode right = new TreeNode(value);
+            node.right = right;
+            populate(sc, right);
+        }
+
+    }
+
+
+    public void prettyDisplay(){
+        prettyDisplay(root, 0);
+    }
+
+    private void prettyDisplay(TreeNode node, int level){
+        if(node == null){
+            return;
+        }
+
+        prettyDisplay(node.right, level+1);
+
+        if(level != 0){
+            for(int i=0; i<level-1; i++){
+                System.out.print("|\t\t");
+            }
+            System.out.println("|------->"+ node.val);
+        }else{
+            System.out.println(node.val);
+        }
+
+        prettyDisplay(node.left, level+1);
+
+    }
+
+    public int diameterOfBinaryTree() {
+        height(root);
+        return diameter;
+    }
+
+    public int height(TreeNode node){
+        if(node == null){
+            return 0;
+        }
+
+        int left = height(node.left);
+        int right = height(node.right);
+
+        diameter = Math.max(diameter, left+right);
+
+        return 1+ Math.max(left, right);
+    }
+
+
+    private static class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int value){
+            this.val = value;
+        }
+    }
+}

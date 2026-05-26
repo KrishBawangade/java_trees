2a5803f25a5e9496b938272acc59c205f3b02829
KrishBawangade <krishbawangade08@gmail.com>
2026-05-26 07:41:43 +0530

    Binary Tree Max Depth Program

---
+++ imp_questions/binary_tree_max_depth/BinaryTree.java
@@ -0,0 +1,91 @@
+package imp_questions.binary_tree_max_depth;
+
+import java.util.Scanner;
+
+public class BinaryTree {
+    TreeNode root;
+
+    public BinaryTree(){
+
+    }
+
+    public void populate(Scanner sc){
+        System.out.print("Enter the root node: ");
+        int value = sc.nextInt();
+        root = new TreeNode(value);
+
+        populate(sc, root);
+    }
+
+    private void populate(Scanner sc, TreeNode node){
+        System.out.printf("Do you want to enter left of %d: ", node.val);
+        boolean isLeft = sc.nextBoolean();
+
+        if(isLeft){
+            System.out.print("Enter the value of the left node: ");
+            int value = sc.nextInt();
+            TreeNode left = new TreeNode(value);
+            node.left = left;
+            populate(sc, left);
+        }
+
+        System.out.printf("Do you want to enter right of %d: ", node.val);
+        boolean isRight = sc.nextBoolean();
+
+        if(isRight){
+            System.out.print("Enter the value of the right node: ");
+            int value = sc.nextInt();
+            TreeNode right = new TreeNode(value);
+            node.right = right;
+            populate(sc, right);
+        }
+
+    }
+
+
+    public void prettyDisplay(){
+        prettyDisplay(root, 0);
+    }
+
+    private void prettyDisplay(TreeNode node, int level){
+        if(node == null){
+            return;
+        }
+
+        prettyDisplay(node.right, level+1);
+
+        if(level != 0){
+            for(int i=0; i<level-1; i++){
+                System.out.print("|\t\t");
+            }
+            System.out.println("|------->"+ node.val);
+        }else{
+            System.out.println(node.val);
+        }
+
+        prettyDisplay(node.left, level+1);
+
+    }
+
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return Math.max(left, right) + 1;
+    }
+
+
+    private static class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int value){
+            this.val = value;
+        }
+    }
+}
